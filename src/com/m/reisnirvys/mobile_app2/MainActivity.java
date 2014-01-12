ddd12e1f22fbb38f0e164eed83a4065e22b70c21
joroni <jraymund.niconi@gmail.com>
2014-01-12 12:52:06 +0800

    Testing 2
    Testing 2

---
+++ src/com/m/reisnirvys/mobile_app2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.m.reisnirvys.mobile_app2;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      /* super.setIntegerProperty(“splashscreen”, R.drawable.splash); */
+        super.loadUrl("file:////android_asset/www/index.html");
+    }
+
+    
+}
+
+
